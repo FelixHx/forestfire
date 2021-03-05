192c787475887b0dbc5d6efe7145fa7c54d87f95
FelixHx <felix@holderied.de>
2021-03-05 11:35:37 +0100

    Create forestfire.sh

---
+++ forestfire.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+LINES=`tput lines` # Number of rows x
+COLS=`tput cols`   # Number of columns y
+F="0.000001"        # Probability for burning tree
+P="0.01"           # Probability for growing tree
+
+clear
+
+awk 'BEGIN{
+  TREE    = "T"
+  BURNING = "w"    # w looks like fire, right?
+  # EMPTY isnt initialized, so it is Null
+  # Get height and width from command line
+  X = ARGV[1]
+  Y = ARGV[2]
+  F = ARGV[3]
+  P = ARGV[4]
+  printf("\033[?25l") # Remove Cursor
+  while(1){
+    # Generate new state depending on old state and random
+    for(x=1; x<=X; x++)
+      for (y=0; y<Y; y++){
+        # Default: state does not change
+        new[x,y]=old[x,y]
+        # Random grow of TREE on EMPTY field
+        if (!old[x,y] && rand()<P){
+          new[x,y]=TREE
+          printf("\033[32m\033["x";"y"HT")
+        }
+        # BURNING tree turns to EMPTY field
+        if (old[x,y]==BURNING){
+          new[x,y]=0
+          printf("\033[33m\033["x";"y"H ")
+        }
+        # TREE starts BURNING randomly or if a neighbour is BURNING
+        if (old[x,y]==TREE)
+          if (rand()<F || match(old[x-1,y] old[x,y-1] old[x+1,y] old[x,y+1], BURNING)){
+             new[x,y]=BURNING
+             printf("\033[31m\033["x";"y"Hw")
+          }
+      }
+    # Copy new state -> old state 
+    for(x=1; x<=X; x++)
+      for (y=0; y<Y; y++)
+        old[x,y]=new[x,y]
+  }
+}' $LINES $COLS $F $P
