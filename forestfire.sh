0a91abbf71c9a25f379d73268b247055d8bbb7f0
Felix Holderied <felix.holderied@opitz-consulting.com>
2021-03-07 22:58:38 +0100

    p

--- forestfire.sh
+++ forestfire.sh
@@ -2,7 +2,7 @@
 
 LINES=`tput lines` # Number of rows x
 COLS=`tput cols`   # Number of columns y
-F="0.00001"        # Probability for burning tree
+F="0.0001"        # Probability for burning tree
 P="0.001"          # Probability for growing tree
 
 clear
